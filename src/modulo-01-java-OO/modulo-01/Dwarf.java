539bb4f4a2a849020cc898fea1eaa76b35294255
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-09-29 13:56:29 -0300

    feat(Dwarf): nomear Dwarf e setar sua data de nascimento.

--- src/modulo-01-java-OO/modulo-01/Dwarf.java
+++ src/modulo-01-java-OO/modulo-01/Dwarf.java
@@ -1,18 +1,34 @@
 public class Dwarf {
     private int vida = 110;
     private String status = "VIVO";
+    private String nome;
+    private DataTerceiraEra dataNascimento;
+
+    public Dwarf (String nome, DataTerceiraEra dataNascimento){
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
     public void tiraVida (){
         vida = vida -10;
         checaVida();
     }
 
-    public void checaVida () {
+    private void checaVida () {
         if (vida <= 0 ){
             status = "MORTO";
             vida = 0;
         }
     }
-
+    
+    public DataTerceiraEra getDataNascimento (){
+        return dataNascimento;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
     public int getVida(){
         return vida;
     }
--- src/modulo-01-java-OO/modulo-01/DwarfTest.java
+++ src/modulo-01-java-OO/modulo-01/DwarfTest.java
@@ -9,7 +9,7 @@ public class DwarfTest
     @Test
     public void testaReceber1Flecha (){
         Elfo elfoDoTeste = new Elfo ("asd");
-        Dwarf zangado = new Dwarf ();
+        Dwarf zangado = new Dwarf ("zangado", new DataTerceiraEra(1,1,1));
         elfoDoTeste.atirarFlecha(zangado);
         assertEquals(100, zangado.getVida());   
     }
@@ -17,7 +17,7 @@ public class DwarfTest
     @Test	
     public void testaDwarfMorto ()  {
         Elfo elfoDoTeste = new Elfo ("asd");
-        Dwarf atchim = new Dwarf ();
+        Dwarf atchim = new Dwarf ("Atchim", new DataTerceiraEra(1,1,1));
         elfoDoTeste.atirarFlecha(atchim);
         elfoDoTeste.atirarFlecha(atchim);
         elfoDoTeste.atirarFlecha(atchim);
@@ -36,7 +36,7 @@ public class DwarfTest
     @Test
     public void testaDwarfVidaNegativa ()  {
         Elfo elfoDoTeste = new Elfo ("asd");
-        Dwarf dunga = new Dwarf ();
+        Dwarf dunga = new Dwarf ("Dunga", new DataTerceiraEra(1,1,1));
         elfoDoTeste.atirarFlecha(dunga);
         elfoDoTeste.atirarFlecha(dunga);
         elfoDoTeste.atirarFlecha(dunga);
@@ -51,5 +51,21 @@ public class DwarfTest
         elfoDoTeste.atirarFlecha(dunga);
         elfoDoTeste.atirarFlecha(dunga);
         assertEquals(0, dunga.getVida());	
+    }
+
+    @Test
+    public void testaDataDeNascimento (){
+        DataTerceiraEra dataEsperada = new DataTerceiraEra(22,1,3987);
+        Dwarf muradinBronzebeard = new Dwarf ("Muradin", dataEsperada);
+        assertEquals(22, muradinBronzebeard.getDataNascimento().getDia());
+        assertEquals(1, muradinBronzebeard.getDataNascimento().getMes());
+        assertEquals(3987, muradinBronzebeard.getDataNascimento().getAno());
+    }
+    
+    @Test
+    public void testaNomeDoDwarf(){
+        String nomeEsperado = "Magni";
+        Dwarf dwarfDoTeste = new Dwarf (nomeEsperado, new DataTerceiraEra(1,1,1));
+        assertEquals("Magni", dwarfDoTeste.getNome());
     }
 }
