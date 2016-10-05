9c4c3c56939fd2999ca7cb9928f0f4ac7fcdfbb7
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-10-05 13:29:09 -0300

    fix(ExercitoDeElfos): corrigir método buscarElfoPeloNome().

--- src/modulo-01-java-OO/modulo-01/ExercitoDeElfos.java
+++ src/modulo-01-java-OO/modulo-01/ExercitoDeElfos.java
@@ -1,26 +1,26 @@
 import java.util.ArrayList;
-public abstract class ExercitoDeElfos{
+
+public class ExercitoDeElfos{
+    
     private ArrayList <Elfo> exercito = new ArrayList<>();
 
     public Elfo buscarElfoPeloNome(String nomeDoElfo){
         for (int i = 0; i < exercito.size(); i ++){            
             Elfo e = exercito.get(i);
-            if (nomeDoElfo.equals(((ElfoNoturno)e).getNome())){
+            if (e instanceof ElfoNoturno && nomeDoElfo.equals(e.getNome())){
                 return (ElfoNoturno)e;
-            } else if (nomeDoElfo.equals(((ElfoVerde)e).getNome())){
+            } else if (e instanceof ElfoVerde && nomeDoElfo.equals(e.getNome())){
                 return (ElfoVerde)e;
             }
         }
-
         return null;
     }
 
-    public void alistarElfo (ElfoNoturno elfo){ // This is a temporary gambi
-        exercito.add(elfo);
-    }
-
-    public void alistarElfo (ElfoVerde elfo){ //This is a temporary gambi
-        exercito.add(elfo);
-    }
-
+    public void alistarElfo (Elfo elfo){
+        if (elfo instanceof ElfoNoturno){
+            exercito.add((ElfoNoturno)elfo);
+        } else if (elfo instanceof ElfoVerde){
+            exercito.add((ElfoVerde)elfo);
+        }
+    }       
 }
