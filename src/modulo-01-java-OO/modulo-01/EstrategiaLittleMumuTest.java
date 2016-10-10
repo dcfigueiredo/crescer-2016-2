36513de98e441b774d2d4d1ce132d7e4b05cbb5f
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-10-09 21:50:53 -0300

    test(EstrategiaLittleMumu): testar se o batalhão estava ordenado de acordo com a quantidade de flechas.

--- src/modulo-01-java-OO/modulo-01/EstrategiaLittleMumuTest.java
+++ src/modulo-01-java-OO/modulo-01/EstrategiaLittleMumuTest.java
@@ -52,13 +52,36 @@ public class EstrategiaLittleMumuTest{
         elfosDoMumuzinho.add(en1);
         elfosDoMumuzinho.add(en2);
         elfosDoMumuzinho.add(elfo);
-        
+
         List <Elfo> listaEstrategica = estrategia.getOrdemDeAtaque(elfosDoMumuzinho);
-        
+
         assertFalse(listaEstrategica.contains(elfo));
         assertEquals(3, listaEstrategica.size());
         assertTrue(listaEstrategica.contains(en1));
         assertFalse(listaEstrategica.contains(en2));
         assertFalse(listaEstrategica.contains(ev1));
+    }
+
+    @Test
+    public void testarElfosOrdenadosDeAcordoComAQuantidadeDeFlechas (){
+        EstrategiaLittleMumu estrategia = new EstrategiaLittleMumu();
+        ElfoVerde ev1 = new ElfoVerde ("Verde 1", 0);          
+        ElfoVerde ev2 = new ElfoVerde ("Verde 2", 40);
+        ElfoVerde ev3 = new ElfoVerde ("Verde 3", 50);
+        ElfoNoturno en1 = new ElfoNoturno ("Noturno 1", 3);
+        ElfoNoturno en2 = new ElfoNoturno ("Noturno 2", 12);
+        List <Elfo> elfosDoMumuzinho = new ArrayList <Elfo>();
+        elfosDoMumuzinho.add(ev1);
+        elfosDoMumuzinho.add(ev2);
+        elfosDoMumuzinho.add(ev3);
+        elfosDoMumuzinho.add(en1);
+        elfosDoMumuzinho.add(en2);
+
+        List <Elfo> listaEstrategica = estrategia.getOrdemDeAtaque(elfosDoMumuzinho);
+
+        assertEquals(ev3, listaEstrategica.get(0));
+        assertEquals(ev2, listaEstrategica.get(1));
+        assertEquals(en1, listaEstrategica.get(2));
+
     }
 }
