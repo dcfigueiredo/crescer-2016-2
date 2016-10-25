6bc73ab9232e54c17c6348b9638613458cc90d03
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-10-25 17:15:13 -0200

    feat(dia-01\Exercicios): resolução exercicios 4 e 5.

--- src/modulo-04/dia-01/dia-01.js
+++ src/modulo-04/dia-01/dia-01.js
@@ -24,20 +24,13 @@ function diggletDig() {
 }
 
 // Exercicio 03
-function Stringuicidade(elem) {
-    return typeof elem === 'string';
-};
-var maiorIgualADois = function (elemento) {
-    return elemento >= 2;
-};
-
-function find(array, funcao) {
+function find(array, fnFiltro) {
     var tamanhoArray = array.length;
     var arrayDeRetorno = [];
-    if (typeof funcao === 'function') { //BONUS 1
+    if (typeof fnFiltro === 'function') { //BONUS 1
         for (var i = 0; i < tamanhoArray; i++) {
             var elemento = array[i];
-            if (funcao(elemento)) {
+            if (fnFiltro(elemento)) {
                 arrayDeRetorno.push(elemento);
             }
         }
@@ -46,33 +39,71 @@ function find(array, funcao) {
 }
 
 //Exercicio 4
+function subtrair(numero) {
+    return function funception(numero2) {
+        return numero - numero2;
+    }
+
+}
 
 //Exercicio 5
 
-var myCar = new Object();
-myCar.make = "Ford";
-myCar.model = "Mustang";
-myCar.year = 1969;
-
-var myCar2 = new Object();
-myCar2.make = "Ford";
-myCar2.model = "Mustang";
-myCar2.year = 1969;
-
-function iguais(objeto1, objeto2) {
-    var ehIgual = false;
-    if (objeto1 === objeto2) {
+function iguais(obj1, obj2) {
+    if (obj1 === obj2) {
         return true;
     }
-    for (var key in objeto1) {
-        if (objeto1[key] === objeto2[key]) {
-            ehIgual = iguais(objeto1[key], objeto2[key]);
-        } else {
+    function ehObjeto(obj) {
+        return typeof obj === "object";
+    }
+    if (ehObjeto(obj1) && ehObjeto(obj2)) {
+
+        function qtdPropriedades (obj){
+            let contador;
+            for (let prop in obj){
+                contador++;
+            }
+            return contador;
+        }
+
+        if (qtdPropriedades(obj1) != qtdPropriedades(obj2)){
             return false;
         }
+
+        for (let prop in obj1) {
+            let igual = iguais(obj1[prop], obj2[prop]);
+            if (!igual) {
+                return false;
+            }
+        }
+        return true
     }
-    return ehIgual;
+    return obj1 === obj2;
 }
+
+// var myCar = new Object();
+// myCar.make = "Ford";
+// myCar.model = "Mustang";
+// myCar.year = 1969;
+
+// var myCar2 = new Object();
+// myCar2.make = "Ford";
+// myCar2.model = "Mustang";
+// myCar2.year = 1969;
+
+// function iguais(objeto1, objeto2) {
+//     var ehIgual = false;
+//     if (objeto1 === objeto2) {
+//         return true;
+//     }
+//     for (var key in objeto1) {
+//         if (objeto1[key] === objeto2[key]) {
+//             ehIgual = iguais(objeto1[key], objeto2[key]);
+//         } else {
+//             return false;
+//         }
+//     }
+//     return ehIgual;
+// }
 
 //Exercicio 6
 
