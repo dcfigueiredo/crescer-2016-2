f8d6422641ff16aa44b17c72083acd5dec038210
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-10-26 01:00:11 -0200

    feat(Marvel): criar método que retorna a média total de páginas de quadrinhos e metodo que retornar series ordenadas pela longevidade decrescente.

--- src/modulo-04/dia-02/daniel-marvel-01.js
+++ src/modulo-04/dia-02/daniel-marvel-01.js
@@ -8,35 +8,80 @@ class Herois {
 
     foraDaGuerraCivil() { //2 
         this.arrayDosPacificos = new Array();
-        for (let prop in this.arrayDeHerois){
+        for (let prop in this.arrayDeHerois) {
             let heroi = this.arrayDeHerois[prop];
             let eventos = heroi.events.items;
             let heroiPacifico = true;
-            for (let key in eventos){
-                let itemEvento = eventos[key];         
-                console.log(itemEvento.name);       
-                if (itemEvento.name === "Civil War"){
+            for (let key in eventos) {
+                let itemEvento = eventos[key];
+                if (itemEvento.name === "Civil War") {
                     heroiPacifico = false;
                 }
             }
-            if (heroiPacifico){
+            if (heroiPacifico) {
                 this.arrayDosPacificos.push(this.arrayDeHerois[prop]);
             }
         }
         return this.arrayDosPacificos;
     }
 
-    maisPublicado (){ //3
+    maisPublicado() { //3
         let maiorQuantidade = 0;
         let heroiComMaisComics;
-        for (let prop in this.arrayDeHerois){
+        for (let prop in this.arrayDeHerois) {
             let heroi = this.arrayDeHerois[prop];
             let numeroDeComics = heroi.comics.available;
-            if (numeroDeComics > maiorQuantidade){
+            if (numeroDeComics > maiorQuantidade) {
                 maiorQuantidade = numeroDeComics;
                 heroiComMaisComics = heroi;
             }
         }
         return heroiComMaisComics;
     }
+
+
+    mediaDePaginas() {
+        let mediaDePaginasDoHeroi = 0;
+        let mediaTotal = 0;
+        for (var prop in this.arrayDeHerois) {
+            let quadrinhos = this.arrayDeHerois[prop].comics.items;
+            for (var key in quadrinhos) {
+                mediaDePaginasDoHeroi += quadrinhos[key].pageCount;
+            }
+            mediaDePaginasDoHeroi = mediaDePaginasDoHeroi / key;
+            mediaTotal += mediaDePaginasDoHeroi;
+        }
+        return Math.round(mediaTotal / prop);
+    }
+
+    seriesPorLongevidade (){
+        let seriesOrdenadas = [];
+        for (let prop in this.arrayDeHerois){
+            let heroi = this.arrayDeHerois[prop];
+            let seriados = heroi.series.items;
+            for(let key in seriados){
+                seriesOrdenadas.push(seriados[key]);
+            }
+        }
+        var trocando = true;
+        
+        //BUBBLESORT :)
+        do{
+            trocando = false;
+            for (let i = 0; i < seriesOrdenadas.length - 1; i ++){
+                let longevidade = seriesOrdenadas[i].endYear - seriesOrdenadas[i].startYear;
+                let longevidadeASerComparada = seriesOrdenadas[i+1].endYear - seriesOrdenadas[i+1].startYear;
+                let precisaTrocar = longevidade < longevidadeASerComparada;
+                if (precisaTrocar){
+                    let serieATrocar = seriesOrdenadas[i];
+                    seriesOrdenadas[i] = seriesOrdenadas[i+1];
+                    seriesOrdenadas[i+1] = serieATrocar;
+                    trocando = true;
+                }
+            }
+
+        } while (trocando);
+        return seriesOrdenadas;
+    }
 }
+
