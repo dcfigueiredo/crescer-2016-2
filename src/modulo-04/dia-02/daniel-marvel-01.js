470a58945ee90f508c893da7d84962ecf19be223
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-10-25 22:17:12 -0200

    feat(Marvel): adicionar método que separa os herois que não participaram da Guerra Civil.

--- src/modulo-04/dia-02/daniel-marvel-01.js
+++ src/modulo-04/dia-02/daniel-marvel-01.js
@@ -1,8 +1,28 @@
 class Herois {
     constructor(array = []) {
         this.arrayDeHerois = new Array();
-        for (let i = 0; i < array.length; i++) {
-            this.arrayDeHerois.push(array[i]);
+        for (let prop in array) {
+            this.arrayDeHerois.push(array[prop]);
         }
+    }
+
+    foraDaGuerraCivil() {
+        this.arrayDosPacificos = [];
+        for (let prop in this.arrayDeHerois){
+            let heroi = this.arrayDeHerois[prop];
+            let eventos = heroi.events.items;
+            let heroiPacifico = true;
+            for (let key in eventos){
+                let itemEvento = eventos[key];         
+                console.log(itemEvento.name);       
+                if (itemEvento.name === "Civil War"){
+                    heroiPacifico = false;
+                }
+            }
+            if (heroiPacifico){
+                this.arrayDosPacificos.push(this.arrayDeHerois[prop]);
+            }
+        }
+        return this.arrayDosPacificos;
     }
 }
