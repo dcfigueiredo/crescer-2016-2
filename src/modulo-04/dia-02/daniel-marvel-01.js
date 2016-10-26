13ed0fcf83550f136dda202f2b33b6533f6d5be4
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-10-25 23:19:22 -0200

    feat(marvel): criar método que retorna o Heroi com maior numero de comics publciadas.

--- src/modulo-04/dia-02/daniel-marvel-01.js
+++ src/modulo-04/dia-02/daniel-marvel-01.js
@@ -1,13 +1,13 @@
 class Herois {
-    constructor(array = []) {
+    constructor(array = []) { //1
         this.arrayDeHerois = new Array();
         for (let prop in array) {
             this.arrayDeHerois.push(array[prop]);
         }
     }
 
-    foraDaGuerraCivil() {
-        this.arrayDosPacificos = [];
+    foraDaGuerraCivil() { //2 
+        this.arrayDosPacificos = new Array();
         for (let prop in this.arrayDeHerois){
             let heroi = this.arrayDeHerois[prop];
             let eventos = heroi.events.items;
@@ -24,5 +24,19 @@ class Herois {
             }
         }
         return this.arrayDosPacificos;
+    }
+
+    maisPublicado (){ //3
+        let maiorQuantidade = 0;
+        let heroiComMaisComics;
+        for (let prop in this.arrayDeHerois){
+            let heroi = this.arrayDeHerois[prop];
+            let numeroDeComics = heroi.comics.available;
+            if (numeroDeComics > maiorQuantidade){
+                maiorQuantidade = numeroDeComics;
+                heroiComMaisComics = heroi;
+            }
+        }
+        return heroiComMaisComics;
     }
 }
