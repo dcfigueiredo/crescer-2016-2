574517e76505df722a8bfd431e36d1f69e2f8470
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-10-27 17:42:06 -0200

    feat(Kart): adicionar classe Kart com Corredor, Velocidade e Equiamentos. WIP

---
+++ src/modulo-05/MarioKart/MarioKart/MarioKart/Karts/Kart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Karts
+{
+    public abstract class Kart
+    {
+
+        public Corredor Corredor { get; private set; }
+        public List<IEquipamentos> Equipamentos { get; private set; }
+        public int Velocidade
+        {
+            get
+            {
+                return this.Velocidade;
+            }
+            private set
+            {
+                
+                int velocidadePadrao = 3;
+                int bonusDeHabilidadeDoCorredor = 0;
+                int bonusDosEquipamentos = somarBonusDeEquipamentos();                
+                if (this.Corredor.Nivel == Corredor.Habilidade.noob)
+                {
+                    bonusDeHabilidadeDoCorredor = 3;
+                } else if (this.Corredor.Nivel == Corredor.Habilidade.mediano)
+                {
+                    bonusDeHabilidadeDoCorredor = 5;
+                } else
+                {
+                    int quantidadeDeEquipamentos = Equipamentos.Count;
+                    bonusDeHabilidadeDoCorredor = 6 + quantidadeDeEquipamentos;
+                }
+
+                this.Velocidade = velocidadePadrao +
+                                  bonusDeHabilidadeDoCorredor +
+                                  bonusDosEquipamentos;
+            }
+        }
+
+        private int somarBonusDeEquipamentos()
+        {
+            int bonusDeEquipamentos = 0;
+            for (int i = 0; i < Equipamentos.Count; i++)
+            {
+                bonusDeEquipamentos += Equipamentos[i].BonusDeVelocidade;
+            }
+            return bonusDeEquipamentos;
+        }
+    }
+}
