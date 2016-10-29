1ad9cf7cf084ab2757b3de82c5863c5b595b2358
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-10-29 20:02:23 -0200

    feat(Equipamentos): criar interface para equipamentos e duas classes para cada tipo de equipamento com diferentes bonus de velocidade.

---
+++ src/modulo-05/MarioKart/MarioKart/Equipamentos/PneusDeCouroDeDragao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Equipamentos
+{
+    public class PneusDeCouroDeDragao : IEquipamento
+    {
+        public PneusDeCouroDeDragao()
+        {
+            this.BonusDeVelocidade = 2;
+        }
+        public int BonusDeVelocidade { get;}
+    }
+}
