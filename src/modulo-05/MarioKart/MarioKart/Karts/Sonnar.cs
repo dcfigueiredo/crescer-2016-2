f9dac3b6350a8af00ebfc49f7076cb6ff7b2ab01
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-10-29 21:36:08 -0200

    feat(Sonnar): adicionar kart que concede +2 de velocidade se possuir pelo menos 1 equipamento.

---
+++ src/modulo-05/MarioKart/MarioKart/Karts/Sonnar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Karts
+{
+    public class Sonnar : Kart
+    {
+        public Sonnar(Corredor corredor) :base(corredor)
+        {
+
+        }
+
+        public override int Velocidade
+        {
+            get
+            {
+                int somaDosBonusDeEquipamento = SomarBonus();                
+                int bonusHabilidadeCorredor = AcharBonusDeHabilidadeDoCorredor();
+                int velocidadeTotal = somaDosBonusDeEquipamento + bonusHabilidadeCorredor;
+                bool temMaisQueUmEquip = Equipamentos.Count > 0;
+                if (temMaisQueUmEquip)
+                {
+                    return 3 + bonusHabilidadeCorredor + somaDosBonusDeEquipamento + 2;
+                } 
+
+                return 3 + bonusHabilidadeCorredor + somaDosBonusDeEquipamento;
+            }
+        }
+    }
+}
---
+++ src/modulo-05/MarioKart/MarioKartTest/SonnarTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart.Equipamentos;
+using MarioKart.Karts;
+
+namespace MarioKartTest
+{
+    [TestClass]
+    public class SonnarTest
+    {
+        [TestMethod]
+        public void TestarSonarCom1Equipamento()
+        {
+            Kart kart = new Sonnar(new MarioKart.Corredor("Megaman", MarioKart.Corredor.NivelDeHabilidade.Mediano));
+            kart.AdicionarEquipamento(new PneusDeCouroDeDragao());
+            int velocidadeEsperada = 12;
+            int velocidadeDoKart = kart.Velocidade;
+
+            Assert.AreEqual(velocidadeEsperada, velocidadeDoKart);
+        }
+
+        [TestMethod]
+        public void TestarSonarCom0Equipamentos()
+        {
+            Kart kart = new Light(new MarioKart.Corredor("Megaman", MarioKart.Corredor.NivelDeHabilidade.Mediano));
+            int velocidadeEsperada = 8;
+            int velocidadeDoKart = kart.Velocidade;
+
+            Assert.AreEqual(velocidadeEsperada, velocidadeDoKart);
+        }
+    }
+}
