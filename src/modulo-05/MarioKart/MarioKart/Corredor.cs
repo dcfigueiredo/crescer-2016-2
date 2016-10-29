fca23f9e6d5fbd9bb055a6b388c0f77a302311ca
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-10-29 20:01:34 -0200

    feat(Corredor): criar classe corredor com diferentes numeros de habilidades.

---
+++ src/modulo-05/MarioKart/MarioKart/Corredor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart
+{
+    public class Corredor
+    {
+        public Corredor(string nome, NivelDeHabilidade habilidade = NivelDeHabilidade.Noob)
+        {
+            this.Nome = nome;
+            this.Habilidade = habilidade;
+        }
+        public enum NivelDeHabilidade { Noob, Mediano, Profissional}
+        public string Nome { get; set; }
+        public NivelDeHabilidade Habilidade { get; set; }
+
+    }
+}
