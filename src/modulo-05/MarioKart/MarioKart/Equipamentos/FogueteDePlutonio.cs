0929a60ad16b5cd87cf02316fe7237d4b4d7668f
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-10-29 21:50:48 -0200

    feat(FogueteDePlutonio): adicionar equipamento foguete de plutonio que concede bonus de velocidade de acordo com o nivel.

---
+++ src/modulo-05/MarioKart/MarioKart/Equipamentos/FogueteDePlutonio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Equipamentos
+{
+    public class FogueteDePlutonio : IEquipamento
+    {
+        public enum Nivel { Um, Dois, Tres, Quatro, Cinco }
+        public FogueteDePlutonio(Nivel nivel)
+        {
+            this.BonusDeVelocidade = IdentificarBonusVelocidade(nivel);
+        }
+
+        public int BonusDeVelocidade { get; }
+
+        private int IdentificarBonusVelocidade(Nivel nivel)
+        {
+            if (nivel == Nivel.Um)
+            {
+                return 1;
+            }
+            else if (nivel == Nivel.Dois)
+            {
+                return 2;
+            }
+            else if (nivel == Nivel.Tres)
+            {
+                return 3;
+            }
+            else if (nivel == Nivel.Quatro)
+            {
+                return 4;
+            }
+            else if (nivel == Nivel.Cinco)
+            {
+                return 5;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+
+    }
+}
---
+++ src/modulo-05/MarioKart/MarioKartTest/FogueteDePlutonioTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart.Equipamentos;
+using MarioKart.Karts;
+
+namespace MarioKartTest
+{
+    [TestClass]
+    public class FogueteDePlutonioTest
+    {
+        [TestMethod]
+        public void TestarLightComFogueteDePlutonioNivel1()
+        {
+            Kart kart = new Light(new MarioKart.Corredor("Megaman", MarioKart.Corredor.NivelDeHabilidade.Noob));
+            kart.AdicionarEquipamento(new FogueteDePlutonio(FogueteDePlutonio.Nivel.Um));
+            int velocidadeEsperada = 10;
+            int velocidadeDoKart = kart.Velocidade;
+
+            Assert.AreEqual(velocidadeEsperada, velocidadeDoKart);
+        }
+
+        [TestMethod]
+        public void TestarSonnarComFogueteDePlutonioNivel5()
+        {
+            Kart kart = new Sonnar(new MarioKart.Corredor("Megaman", MarioKart.Corredor.NivelDeHabilidade.Profissional));
+            kart.AdicionarEquipamento(new FogueteDePlutonio(FogueteDePlutonio.Nivel.Cinco));
+            int velocidadeEsperada = 17;
+            int velocidadeDoKart = kart.Velocidade;
+
+            Assert.AreEqual(velocidadeEsperada, velocidadeDoKart);
+        }
+    }
+}
