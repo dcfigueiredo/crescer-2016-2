ac53c077f29d75a31f63c5748e77d2a6a13d7b3a
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-11-05 23:18:10 -0200

    refactor(Aplicativo/PersonagemAplicativo): mover lógica de decisao entre editar ou incluir para o repositorio.

--- src/modulo-05/aula-03/StreetFighter.Web/StreetFighter.Aplicativo/PersonagemAplicativo.cs
+++ src/modulo-05/aula-03/StreetFighter.Web/StreetFighter.Aplicativo/PersonagemAplicativo.cs
@@ -29,10 +29,12 @@ namespace StreetFighter.Aplicativo
 
         public void Salvar(Personagem personagem)
         {
-            if (personagem.Id == 0)
-                repositorio.IncluirPersonagem(personagem);
-            else
-                repositorio.EditarPersonagem(personagem);
+            repositorio.IncluirPersonagem(personagem);
+        }
+
+        public void Excluir(Personagem personagem)
+        {
+            repositorio.ExcluirPersonagem(personagem);
         }
     }
 }
