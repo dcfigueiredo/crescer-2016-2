f71d293aa003bafe5c27265ab6d1bd6c87a33352
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-11-09 18:11:06 -0200

    feat(Loja/ProdutoRepositorio): Adicionar lógica para manipular tabela de produtos

---
+++ src/modulo-05/aula-09/Loja/Loja.Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using Loja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Repositorio
+{
+    public class ProdutoRepositorio : IProdutoRepositorio
+    {
+        public void CadastrarProduto(Produto produto)
+        {
+            using (var context = new ContextoDeDados())
+            {
+                context.Entry<Produto>(produto).State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }        
+
+        public void EditarProduto(int id)
+        {
+            throw new NotImplementedException();
+        }        
+
+        public void ExcluirProduto(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Produto> ListarProdutos()
+        {
+            using (var context = new ContextoDeDados())
+            {
+                return context.Produto.ToList();
+            }
+        }
+    }
+}
