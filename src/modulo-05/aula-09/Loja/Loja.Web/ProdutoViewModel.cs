dc98f032701eb976f8bee383c9d131443d49b71c
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-11-09 18:14:33 -0200

    feat(Loja/Web): Criar página para listagem e cadastro de produtos.

---
+++ src/modulo-05/aula-09/Loja/Loja.Web/ProdutoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Loja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Loja.Web
+{
+    public class ProdutoViewModel
+    {
+        public ProdutoViewModel(IList<Produto> listaDeProdutos)
+        {
+            this.ListaDeProdutos = listaDeProdutos;
+        }
+        public IList<Produto> ListaDeProdutos { get; set; }
+    }
+}
