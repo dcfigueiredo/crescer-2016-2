cf5c4d1e7bde23aad99563c764d39f608fe60c36
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-11-25 09:34:43 -0200

    feat(aula1-tema): método que retorna a diferença de anos meses e dias entre duas datas.

--- src/modulo-09/aula1/src/main/java/br/com/cwi/crescer/aula1/MeuCalendarioUtil.java
+++ src/modulo-09/aula1/src/main/java/br/com/cwi/crescer/aula1/MeuCalendarioUtil.java
@@ -17,15 +17,50 @@ public class MeuCalendarioUtil {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Data do nascimento: ");
-        String dataNascimento = scanner.nextLine();
+        String data = scanner.nextLine();
 
-        System.out.println("Dia em que nasceu: " + exibirDiaNascimento(dataNascimento));
-                
+        System.out.println("Dia em que nasceu: " + exibirDiaNascimento(data));
+        
+        System.out.print("Digite a data para a qualquer saber a diferença: ");
+        data = scanner.nextLine();
+        
+        diferencaDeData(data);
+
+    }
+    
+    //TO-DO: Achar uma maneira mais elegante de fazer isso
+    public static String diferencaDeData(String data) {
+        DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Calendar antes = Calendar.getInstance();
+        Calendar agora = Calendar.getInstance();
+
+        try {
+            Date date = sdf.parse(data);
+            antes.setTime(date);
+        } catch (Exception ex) {
+            System.out.println("Data errada." + ex);
+        }
+
+        int anoAntes = antes.get(Calendar.YEAR);
+        int mesAntes = antes.get(Calendar.MONTH);
+        int diaAntes = antes.get(Calendar.DATE);
+
+        int anoAgora = agora.get(Calendar.YEAR);
+        int mesAgora = agora.get(Calendar.MONTH);
+        int diaAgora = agora.get(Calendar.DATE);
+
+        int difAno = anoAgora - anoAntes;
+        int difMes = mesAgora - mesAntes;
+        int difDia = diaAgora - diaAntes;
+
+        System.out.println(difAno > 1 ? difAno + " Anos" : difAno + " Ano");
+        System.out.println(difMes > 1 ? difMes + " Meses" : difMes + " Mes");
+        System.out.println(difDia > 1 ? difDia + " Dias" : difDia + " Dia");
+
+        return null;
     }
 
-//    public static String diferencaDeData(String data){        
-//    }
-    
     public static String exibirDiaNascimento(String dataNascimento) {
         DateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
 
