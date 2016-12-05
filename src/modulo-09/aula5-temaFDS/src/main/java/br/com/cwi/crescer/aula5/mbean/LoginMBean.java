624d2d721d6b596a766207fd8df72e42d1437bc2
Unknown <daniel.carvalho.figueiredo@gmail.com>
2016-12-05 09:08:20 -0200

    feat(aula5temaFDS): implementa método que retorna bean da classe LoginMBean

--- src/modulo-09/aula5-temaFDS/src/main/java/br/com/cwi/crescer/aula5/mbean/LoginMBean.java
+++ src/modulo-09/aula5-temaFDS/src/main/java/br/com/cwi/crescer/aula5/mbean/LoginMBean.java
@@ -73,7 +73,7 @@ public class LoginMBean extends AbstractMBean<Usuario, Long, UsuarioDao, Usuario
 
     @Override
     public UsuarioBean getBean() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return bean;
     }
 
     @Override
